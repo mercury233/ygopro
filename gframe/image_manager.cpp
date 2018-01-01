9637022ab26eab9fdd3ad6381612715165b498f2
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-01-02 00:14:24 +0800

    Update image_manager.cpp

--- gframe/image_manager.cpp
+++ gframe/image_manager.cpp
@@ -38,6 +38,20 @@ bool ImageManager::Initial()  {
 	tFieldTransparent[0] = driver->getTexture("textures/field-transparent2.png");
 	tField[1] = driver->getTexture("textures/field3.png");
 	tFieldTransparent[1] = driver->getTexture("textures/field-transparent3.png");
+	int i = 0;
+	char buff[100];
+	for (; i < 14; i++) {
+		snprintf(buff, 100, "/textures/extra/rscale_%d.png", i);
+		tRScale[i] = driver->getTexture(path(buff));
+	}
+	for (i = 0; i < 14; i++) {
+		snprintf(buff, 100, "/textures/extra/lscale_%d.png", i);
+		tLScale[i] = driver->getTexture(path(buff));
+	}
+	support_types.push_back(std::string("jpg"));
+	support_types.push_back(std::string("png"));
+	support_types.push_back(std::string("bmp"));
+	return true;
 	return true;
 }
 void ImageManager::SetDevice(irr::IrrlichtDevice* dev) {
