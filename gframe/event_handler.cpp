d62be38206bd155a0807a122aade13c13d2d0b3b
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-01-28 23:25:02 +0800

    Update event_handler.cpp

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -2000,7 +2000,7 @@ bool ClientField::OnCommonEvent(const irr::SEvent& event) {
 				const wchar_t* pstr = mainGame->ebSearchName->getText();
 				BufferIO::CopyWStr(pstr, searchname, 256);
 				char buffer[300] = {0};
-				sprintf(buffer, "start /b www.ourocg.cn/S.aspx?key=%s", pstr);
+				sprintf(buffer, "start /b www.ourocg.cn/S.aspx?key=%s", searchname);
 				system(buffer);
 				return true;
 				break;
