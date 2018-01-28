d994a28a4789f68e5425eb5a7fe4fb6c55467fbf
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-01-28 22:29:27 +0800

    Update game.h

--- gframe/game.h
+++ gframe/game.h
@@ -245,7 +245,7 @@ public:
 	irr::gui::IGUIScrollBar* scrMusicVolume;
 	irr::gui::IGUICheckBox* chkMusicMode;
 	irr::gui::IGUICheckBox* chkEnablePScale;
-	irr::gui::IGUIWindow* wSearchMenu;
+	irr::gui::IGUIWindow* wSearchWindow;
 	irr::gui::IGUIButton* btnCardSearch;
 	irr::gui::IGUIButton* btnSearchAgree;
 	irr::gui::IGUIButton* btnSearchCancel;
