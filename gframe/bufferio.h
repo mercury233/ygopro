381d77c83e29c6b35f411d3f38102ba293988997
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-01-29 01:14:31 +0800

    Update bufferio.h

--- gframe/bufferio.h
+++ gframe/bufferio.h
@@ -106,6 +106,36 @@ public:
 		*wp = 0;
 		return wp - wstr;
 	}
+	// URLEncode
+	static int URLEncode(const char* str, const int strSize, char* result, const int resultSize) {  
+               int i;  
+               int j = 0;//for result index  
+               char ch;  
+               if ((str==NULL) || (result==NULL) || (strSize<=0) || (resultSize<=0)) {  
+               return 0;  
+               }  
+               for ( i=0; (i<strSize)&&(j<resultSize); ++i) {  
+               ch = str[i];  
+                  if (((ch>='A') && (ch<'Z')) ||  
+                  ((ch>='a') && (ch<'z')) ||  
+                  ((ch>='0') && (ch<'9'))) {  
+                  result[j++] = ch;  
+                  } else if (ch == ' ') {  
+                  result[j++] = '+';  
+                  } else if (ch == '.' || ch == '-' || ch == '_' || ch == '*') {  
+                  result[j++] = ch;  
+                  } else {  
+                if (j+3 < resultSize) {  
+                sprintf(result+j, "%%%02X", (unsigned char)ch);  
+                j += 3;  
+            } else {  
+            return 0;  
+            }  
+        }  
+    } 
+    result[j] = '\0';  
+    return j;  
+}   
 	static int GetVal(const wchar_t* pstr) {
 		int ret = 0;
 		while(*pstr >= L'0' && *pstr <= L'9') {
