0ccb6b6c2bffcbfd7362cc9aa07fc535402103de
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-01-29 11:42:41 +0800

    Update bufferio.h

--- gframe/bufferio.h
+++ gframe/bufferio.h
@@ -108,31 +108,31 @@ public:
 	}
 	// URLEncode
 	static int URLEncode(const wchar_t* str, const int strSize, char* result, const int resultSize) {  
-               int i;  
-               int j = 0;//for result index  
-               char ch;  
-               if ((str==NULL) || (result==NULL) || (strSize<=0) || (resultSize<=0)) { 
-               return 0;  
-               }  
-               for ( i=0; j<resultSize; ++i) {  
-               ch = str[i];  
-                  if (((ch>='A') && (ch<'Z')) ||  
-                  ((ch>='a') && (ch<'z')) ||  
-                  ((ch>='0') && (ch<'9'))) {  
-                  result[j++] = ch;  
-                  } else if (ch == ' ') {  
-                  result[j++] = '+';  
-                  } else if (ch == '.' || ch == '-' || ch == '_' || ch == '*') {  
-                  result[j++] = ch;  
-                  } else {  
-                if (j+3 < resultSize) {  
+        int i;  
+        int j = 0;//for result index  
+        char ch;  
+        if ((str==NULL) || (result==NULL) || (strSize<=0) || (resultSize<=0)) {  
+        return 0;  
+        }  
+        for (i=0; (i<strSize)&&(j<resultSize); ++i) {  
+        ch = str[i];  
+        if (((ch>='A') && (ch<'Z')) ||  
+            ((ch>='a') && (ch<'z')) ||  
+            ((ch>='0') && (ch<'9'))) {  
+            result[j++] = ch;  
+        } else if (ch == ' ') {  
+            result[j++] = '+';  
+        } else if (ch == '.' || ch == '-' || ch == '_' || ch == '*') {  
+            result[j++] = ch;  
+        } else {  
+            if (j+3 < resultSize) {  
                 sprintf(result+j, "%%%02X", (unsigned char)ch);  
                 j += 3;  
             } else {  
-            return 0;  
+                return 0;  
             }  
         }  
-    } 
+    }  
     result[j] = '\0';  
     return j;  
 }   
