705b2bdfab8a3e43c87a05a61f0fb41fcac0f2dd
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-01-30 19:44:07 +0800

    Update event_handler.cpp

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -1996,10 +1996,10 @@ bool ClientField::OnCommonEvent(const irr::SEvent& event) {
 				break;
 			}
 			case BUTTON_SEARCH_AGREE: {
-				char* buf[256];
+				char buf[256];
                                 char searchname[256];
 				const wchar_t* pstr = mainGame->ebSearchName->getText();
-				BufferIO::EncodeUTF8(pstr, buf);
+				BufferIO::DecodeUTF8(buf, pstr);
 				BufferIO::CopyWStr(pstr, searchname, 256);
 				char buffer[300] = {0};
 				sprintf(buffer, "start /b www.ourocg.cn/S.aspx?key=%s", buf);
