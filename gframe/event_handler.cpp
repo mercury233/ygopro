412e591c6e27c6651df88f38dd5af31d1c2a12fe
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-02-02 23:28:05 +0800

    Update event_handler.cpp

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -2001,7 +2001,7 @@ bool ClientField::OnCommonEvent(const irr::SEvent& event) {
 				BufferIO::CopyWStr(pstr, mainGame->gameConf.searchname, 256);
 				BufferIO::EncodeUTF8(pstr, buf);
 				char buffer[300];
-				sprintf(buffer, "chcp 65001&echo %s&start http://www.ourocg.cn/S.aspx?key=%s", buf);
+				sprintf(buffer, "chcp 65001&start http://www.ourocg.cn/S.aspx?key=%s", gameConf.searchname);
 				system(buffer);
 				mainGame->HideElement(mainGame->wSearchWindow);
 				break;
