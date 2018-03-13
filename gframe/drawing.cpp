f0731c8bee3f73a1701396198c13dea3da8b6306
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-03-13 22:39:52 +0800

    Update drawing.cpp

--- gframe/drawing.cpp
+++ gframe/drawing.cpp
@@ -327,7 +327,7 @@ void Game::DrawCard(ClientCard* pcard) {
 		driver->drawVertexPrimitiveList(matManager.vCardFront, 4, matManager.iRectangle, 2);
 	}
 	if(m22 < 0.99 || pcard->is_moving) {
-		matManager.mCard.setTexture(0, imageManager.tCover[pcard->player]);
+		matManager.mCard.setTexture(0, imageManager.tCover[player]);
 		driver->setMaterial(matManager.mCard);
 		driver->drawVertexPrimitiveList(matManager.vCardBack, 4, matManager.iRectangle, 2);
 	}
