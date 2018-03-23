342ea67de1d2884b4968672cc8d2d37682e71b26
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-03-24 00:43:29 +0800

    Update drawing.cpp

--- gframe/drawing.cpp
+++ gframe/drawing.cpp
@@ -959,10 +959,12 @@ void Game::DrawBackImage(irr::video::ITexture* texture) {
 }
 void Game::DrawYFace(irr::video::ITexture* texture) {
 	if(!texture)
+		return;
 	driver->draw2DImage(imageManager.tYFace[0], recti(330, 45, 378, 93), recti(0, 0, 48, 48), 0, 0, true);
 }
 void Game::DrawDFace(irr::video::ITexture* texture) {
 	if(!texture)
+		return;
 	driver->draw2DImage(imageManager.tDFace[1], recti(942, 45, 990, 93), recti(0, 0, 48, 48), 0, 0, true);
 }
 void Game::ShowElement(irr::gui::IGUIElement * win, int autoframe) {
