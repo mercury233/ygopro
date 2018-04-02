743a2a7dc1e5dcc210796e9dfb84e174bb02652e
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-04-02 14:12:31 +0800

    Update deck_con.cpp

--- gframe/deck_con.cpp
+++ gframe/deck_con.cpp
@@ -197,7 +197,8 @@ bool DeckBuilder::OnEvent(const irr::SEvent& event) {
 						myswprintf(newname, L"%ls", mainGame->ebREName->getText());
 					}
 					if(DeckManager::RenameDeck(mainGame->cbDBDecks->getItem(prev_sel), newname)) {
-						mainGame->RefreshDeck(mainGame->cbDBDecks->setSelected(prev_sel));
+						mainGame->RefreshDeck(mainGame->cbDBDecks);
+						mainGame->cbDBDecks->setSelected(prev_sel)
 						mainGame->stACMessage->setText(dataManager.GetSysString(1366));
 					        mainGame->PopupElement(mainGame->wACMessage, 20);
 					} else {
