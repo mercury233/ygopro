cbfe94574ec805b2afb1aba24a700fe1d5836b88
ViolentIris <2571845421@qq.com>
2018-04-06 23:40:55 +0800

    event

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -213,7 +213,17 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 					mainGame->HideElement(mainGame->wQuery, true);
 					break;
 				}
-                case MSG_SELECT_UNSELECT_CARD: 
+				case MSG_SELECT_UNSELECT_CARD: {
+                   if (select_cancelable) {
+                       DuelClient::SetResponseI(-1);
+                       ShowCancelOrFinishButton(0);
+                       if (mainGame->wCardSelect->isVisible())
+                       mainGame->HideElement(mainGame->wCardSelect, true);
+                       else
+                       DuelClient::SendResponse();
+                      }
+                break;
+                } 
 				case MSG_SELECT_CARD: {
 					if (selected_cards.size() == 0) {
 						if (select_cancelable) {
@@ -1629,17 +1639,17 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 				}
 				break;
 			}
-		        case MSG_SELECT_UNSELECT_CARD: {
-                                if (select_cancelable) {
-                                       DuelClient::SetResponseI(-1);
-                                       ShowCancelOrFinishButton(0);
-                                       if (mainGame->wCardSelect->isVisible())
-                                                mainGame->HideElement(mainGame->wCardSelect, true);
-                                       else
-                                                DuelClient::SendResponse();
-                                }
-                        break;
-                        }
+		    case MSG_SELECT_UNSELECT_CARD: {
+                   if (select_cancelable) {
+                       DuelClient::SetResponseI(-1);
+                       ShowCancelOrFinishButton(0);
+                       if (mainGame->wCardSelect->isVisible())
+                       mainGame->HideElement(mainGame->wCardSelect, true);
+                       else
+                       DuelClient::SendResponse();
+                }
+                break;
+            }
 			case MSG_SELECT_TRIBUTE: {
 				if(selected_cards.size() == 0) {
 					if(select_cancelable) {
