84274a20a3c476581c6f3ec1f49b7668d38b3380
VI-1911 <32847546+ViolentIris@users.noreply.github.com>
2018-04-11 13:06:37 +0800

    Update event_handler.h

--- gframe/event_handler.h
+++ gframe/event_handler.h
@@ -8,110 +8,106 @@
 #include <urlmon.h>
 #pragma comment (lib, "urlmon.lib")
 
-HRESULT ( STDMETHODCALLTYPE QueryInterface )( 
-    IBindStatusCallback * This,
-    /* [in] */ REFIID riid,
-    /* [iid_is][out] */ void **ppvObject)
+class CBindCallback : public IBindStatusCallback  
 {
-    return E_NOTIMPL;
-}
+public:
+CBindCallback();
+virtual ~CBindCallback();
 
-ULONG ( STDMETHODCALLTYPE AddRef )( 
-    IBindStatusCallback * This)
-{
-    return E_NOTIMPL;
-}
+     CUrlDownloadToFileCallbackTestDlg* m_pdlg; 
+    
+STDMETHOD(OnStartBinding)
+( DWORD dwReserved,
+IBinding __RPC_FAR *pib)
+{ return E_NOTIMPL; }
 
-ULONG ( STDMETHODCALLTYPE Release )( 
-    IBindStatusCallback * This)
-{
-    return E_NOTIMPL;
-}
+STDMETHOD(GetPriority)
+( LONG __RPC_FAR *pnPriority)
+{ return E_NOTIMPL; }
 
-HRESULT ( STDMETHODCALLTYPE OnStartBinding )( 
-    IBindStatusCallback * This,
-    /* [in] */ DWORD dwReserved,
-    /* [in] */ IBinding *pib)
-{
-    return E_NOTIMPL;
-}
+STDMETHOD(OnLowResource)
+( DWORD reserved)
+{ return E_NOTIMPL; }
 
-HRESULT ( STDMETHODCALLTYPE GetPriority )( 
-    IBindStatusCallback * This,
-    /* [out] */ LONG *pnPriority)
-{
-    return E_NOTIMPL;
-}
+STDMETHOD(OnProgress)
+( ULONG ulProgress,    
+ULONG ulProgressMax,
+ULONG ulStatusCode,
+LPCWSTR wszStatusText);
 
-HRESULT ( STDMETHODCALLTYPE OnLowResource )( 
-    IBindStatusCallback * This,
-    /* [in] */ DWORD reserved)
-{
-    return E_NOTIMPL;
-}
+STDMETHOD(OnStopBinding)
+( HRESULT hresult,
+LPCWSTR szError)
+{ return E_NOTIMPL; }
 
-HRESULT ( STDMETHODCALLTYPE OnProgress )( 
-    IBindStatusCallback * This,
-    /* [in] */ ULONG ulProgress,
-    /* [in] */ ULONG ulProgressMax,
-    /* [in] */ ULONG ulStatusCode,
-    /* [in] */ LPCWSTR szStatusText)
-{
-    if (ulProgress > 0)
-    {   
-        printf("Downloaded %d of %d bytes\n",
-            ulProgress, ulProgressMax); 
-        fflush(stdout);
-    }
-    return S_OK;
-}
+STDMETHOD(GetBindInfo)
+( DWORD __RPC_FAR *grfBINDF,
+BINDINFO __RPC_FAR *pbindinfo)
+{ return E_NOTIMPL; }
 
-HRESULT ( STDMETHODCALLTYPE OnStopBinding )( 
-    IBindStatusCallback * This,
-    /* [in] */ HRESULT hresult,
-    /* [unique][in] */ LPCWSTR szError)
-{
-    return E_NOTIMPL;
-}
+STDMETHOD(OnDataAvailable)
+( DWORD grfBSCF,
+DWORD dwSize,
+FORMATETC __RPC_FAR *pformatetc,
+STGMEDIUM __RPC_FAR *pstgmed)
+{ return E_NOTIMPL; }
 
-HRESULT ( STDMETHODCALLTYPE GetBindInfo )( 
-    IBindStatusCallback * This,
-    /* [out] */ DWORD *grfBINDF,
-    /* [unique][out][in] */ BINDINFO *pbindinfo)
-{
-    return E_NOTIMPL;
-}
+STDMETHOD(OnObjectAvailable)
+( REFIID riid,
+IUnknown __RPC_FAR *punk)
+{ return E_NOTIMPL; }
 
-HRESULT ( STDMETHODCALLTYPE OnDataAvailable )( 
-    IBindStatusCallback * This,
-    /* [in] */ DWORD grfBSCF,
-    /* [in] */ DWORD dwSize,
-    /* [in] */ FORMATETC *pformatetc,
-    /* [in] */ STGMEDIUM *pstgmed)
-{
-    return E_NOTIMPL;
-}
+STDMETHOD_(ULONG,AddRef)()
+{ return 0; }
 
-HRESULT ( STDMETHODCALLTYPE OnObjectAvailable )( 
-    IBindStatusCallback * This,
-    /* [in] */ REFIID riid,
-    /* [iid_is][in] */ IUnknown *punk)
-{
-    return E_NOTIMPL;
-}
+STDMETHOD_(ULONG,Release)()
+{ return 0; }
 
-static const IBindStatusCallbackVtbl* IBindStatusCallback_Vtbl = {
-    QueryInterface,
-    AddRef,
-    Release,
-    OnStartBinding,
-    GetPriority,
-    OnLowResource,
-    OnProgress,
-    OnStopBinding,
-    GetBindInfo,
-    OnDataAvailable,
-    OnObjectAvailable
+STDMETHOD(QueryInterface)
+( REFIID riid,
+void __RPC_FAR *__RPC_FAR *ppvObject)
+{ return E_NOTIMPL; }
 };
+
+CBindCallback::CBindCallback()
+{
+
+}
+
+CBindCallback::~CBindCallback()
+{
+
+}
+
+LRESULT CBindCallback::OnProgress(ULONG ulProgress,
+ULONG ulProgressMax,
+ULONG ulSatusCode,
+LPCWSTR szStatusText)
+{
+CProgressCtrl* m_prg = (CProgressCtrl*)m_pdlg->GetDlgItem(IDC_PROGRESS);
+m_prg->SetRange32(0,ulProgressMax);
+m_prg->SetPos(ulProgress);
+
+CString szText;
+szText.Format("已下载%d%%", (int)(ulProgress * 100.0 / ulProgressMax));
+(m_pdlg->GetDlgItem(IDC_STATUS))->SetWindowText(szText);
+
+return S_OK;
+}
+
+void CUrlDownloadToFileCallbackTestDlg::DownloadThread()
+{
+CBindCallback cbc;
+cbc.m_pdlg = this;
+CString szURL;
+m_url.GetWindowText(szURL);
+m_url.EnableWindow(FALSE);
+(this->GetDlgItem(IDC_START))->EnableWindow(FALSE);
+CString szRand;
+szRand.Format(_T("?skq=%d"),GetTickCount());
+szUrl += szRand;
+if(S_OK == URLDownloadToFile(NULL,szURL,szPath,0,&cbc))
+MessageBox("finished");
+}
 
 #endif //EVENT_HANDLER_H
