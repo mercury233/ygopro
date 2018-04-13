cbcfe98d73aefb8bb1d0d5981055eda30ee090a0
ViolentIris <2571845421@qq.com>
2018-04-13 20:54:10 +0800

    cd

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -1583,7 +1583,7 @@ void Game::OnResize() {
 	}
 
 	wCardImg->setRelativePosition(Resize(1, 1, 1 + CARD_IMG_WIDTH + 20, 1 + CARD_IMG_HEIGHT + 18));
-	imgCard->setRelativePosition(Resize(10, 9, 10 + CARD_IMG_WIDTH, 9 + CARD_IMG_HEIGHT));
+	imgCard->setRelativePosition(Resize(10, 9, CARD_IMG_WIDTH - 21, 9 + CARD_IMG_HEIGHT));
 	wInfos->setRelativePosition(Resize(1, 275, 301, 639));
 	stName->setRelativePosition(recti(10, 10, 287 * xScale, 32));
 	lstLog->setRelativePosition(Resize(10, 10, 290, 290));
