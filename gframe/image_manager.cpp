a7129bef316f133d986519cb0f5f842d5b444af7
ViolentIris <2571845421@qq.com>
2018-05-25 19:27:10 +0800

    .

--- gframe/image_manager.cpp
+++ gframe/image_manager.cpp
@@ -16,7 +16,7 @@ bool ImageManager::Initial()  {
 		const wchar_t* pstl = mainGame->stHostPrepDuelist[h]->getText();
 		BufferIO::EncodeUTF8(pstl, head);
 		char hair[100];
-		snprintf(hair, 100, "textures/head/head_%1s.jpg", head);
+		sprintf(hair, "textures/head/head_%1s.jpg", head);
 		tHead[0] = driver->getTexture(hair);
 		tHead[1] = driver->getTexture(hair);
 	}
