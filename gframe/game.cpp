072edf932e0aa2c812b092464a3eee5b9ebb30cc
ViolentIris <2571845421@qq.com>
2018-07-05 00:00:38 +0800

    .

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -1378,17 +1378,12 @@ void Game::ShowCardInfo(int code, bool resize) {
 		}
 		stDataInfo->setText(formatBuffer);
 		int offset_arrows = 0;
-		if((cd.type) & (TYPE_LINK) && (cd.level > 5)) 
-		offset_arrows = 15;
+		if(cd.type & TYPE_LINK && cd.level > 5 && window_size.Width < 1220.0)
+			offset_arrows = 15;
 		stDataInfo->setRelativePosition(rect<s32>(15, 60, 300 * xScale - 13, (90 + offset_arrows)));
-		stSetName->setRelativePosition(rect<s32>(15, (90 + offset_arrows), 296 * xScale, (83 + offset_arrows) + offset));
-		stText->setRelativePosition(rect<s32>(15, (90 + offset_arrows) + offset, 287 * xScale, 324 * yScale));
-		scrCardText->setRelativePosition(rect<s32>(287 * xScale - 20, (90 + offset_arrows) + offset, 287 * xScale, 324 * yScale));
-		if((cd.type) & (TYPE_LINK) && (cd.level < 6)) 
-		stDataInfo->setRelativePosition(rect<s32>(15, 60, 296, 83));
-		stSetName->setRelativePosition(rect<s32>(15, 83, 296 * xScale, 106));
-		stText->setRelativePosition(rect<s32>(15, 83 + offset, 287 * xScale, 324 * yScale));
-		scrCardText->setRelativePosition(rect<s32>(287 * xScale - 20, 83 + offset, 287 * xScale, 324 * yScale));
+		stSetName->setRelativePosition(rect<s32>(15, (92 + offset_arrows), 296 * xScale, (92 + offset_arrows) + offset));
+		stText->setRelativePosition(rect<s32>(15, (92 + offset_arrows) + offset, 287 * xScale, 324 * yScale));
+		scrCardText->setRelativePosition(rect<s32>(287 * xScale - 20, (83 + offset_arrows) + offset, 287 * xScale, 324 * yScale));
 	} else {
 		myswprintf(formatBuffer, L"[%ls]", dataManager.FormatType(cd.type));
 		stInfo->setText(formatBuffer);
