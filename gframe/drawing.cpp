39ba2d2cb56a01bffce09d4f4e37876894f30bef
ViolentIris <2571845421@qq.com>
2018-08-13 19:40:57 +0800

    .

--- gframe/drawing.cpp
+++ gframe/drawing.cpp
@@ -352,7 +352,7 @@ void Game::DrawCard(ClientCard* pcard) {
 		driver->drawVertexPrimitiveList(matManager.vCardFront, 4, matManager.iRectangle, 2);
 	}
 	if(m22 < 0.99 || pcard->is_moving) {
-		matManager.mCard.setTexture(0, imageManager.tCover[selectable_cards[i]->controler + 2]);
+		matManager.mCard.setTexture(0, imageManager.tCover[pcard->controler]);
 		driver->setMaterial(matManager.mCard);
 		driver->drawVertexPrimitiveList(matManager.vCardBack, 4, matManager.iRectangle, 2);
 	}
