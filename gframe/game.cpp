f1626c65ece176940f7b02d4605b86162bd6d932
VI-1911 <2571845421@qq.com>
2020-02-14 18:34:45 +0800

    .

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -482,7 +482,7 @@ bool Game::Initialize() {
 	posY += 30;
 	chkMusicMode = env->addCheckBox(false, rect<s32>(posX, posY, posX + 260, posY + 25), tabSystem, -1, dataManager.GetSysString(1281));
 	chkMusicMode->setChecked(gameConf.music_mode != 0);
-	btnCardSearch = env->addButton(rect<s32>(125, 55, 225, 105), tabSystem, BUTTON_CARD_SEARCH, dataManager.GetSysString(1283));
+	btnCardSearch = env->addButton(rect<s32>(130, 35, 230, 85), tabSystem, BUTTON_CARD_SEARCH, dataManager.GetSysString(1283));
 	wSearchWindow = env->addWindow(rect<s32>(510, 200, 820, 320), false, dataManager.GetSysString(1284));
 	wSearchWindow->getCloseButton()->setVisible(false);
 	wSearchWindow->setVisible(false);
