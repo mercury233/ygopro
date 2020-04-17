abab56e73d08b5e06db6f64cf8797b56fdefb5cc
VI-1911 <2571845421@qq.com>
2020-04-17 19:32:15 +0800

    .

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -50,7 +50,7 @@ bool Game::Initialize() {
 			if(gameConf.skin_index < 0)
 				index = rand() % count;
 			else if((size_t)gameConf.skin_index <= skins.size())
-				index = gameConf.skin_index - 1;
+				index = skins.size() - gameConf.skin_index;
 			if(index >= 0)
 				skinSystem->applySkin(skins[index].c_str());
 		}
