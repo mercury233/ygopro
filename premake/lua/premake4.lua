00cfe9694170979abb713241e3cca490db4604c5
VI-1911 <2571845421@qq.com>
2021-12-15 19:51:33 +0800

    .

--- premake/lua/premake4.lua
+++ premake/lua/premake4.lua
@@ -2,7 +2,7 @@ project "lua"
     kind "StaticLib"
 
     files { "src/*.c", "src/*.cpp", "src/*.h", "src/*.hpp" }
-    removefiles { "src/lua.c", "src/luac.c" }
+    removefiles { "src/lua.c", "src/luac.c", "src/lua.h"}
 
     configuration "vs*"	
         buildoptions { "/TP" }
