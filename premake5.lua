daa6db0d266f5e9b2b681eca74c2e023dc37a96f
mercury233 <me@mercury233.me>
2025-03-15 15:58:07 +0800

    test

--- premake5.lua
+++ premake5.lua
@@ -84,7 +84,7 @@ elseif GetParam("no-build-freetype") then
     BUILD_FREETYPE = false
 end
 if not BUILD_FREETYPE then
-    FREETYPE_INCLUDE_DIR = GetParam("freetype-include-dir") or os.findheader("freetype")
+    FREETYPE_INCLUDE_DIR = GetParam("freetype-include-dir") or os.findheader("freetype2")
     FREETYPE_LIB_DIR = GetParam("freetype-lib-dir") or os.findlib("freetype")
 end
 
