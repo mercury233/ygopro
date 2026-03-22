e4b6cb1c805a50f5a3e1218385aeb1c9fe737760
mercury233 <me@mercury233.me>
2026-03-22 20:14:05 +0800

    add ImageUtility::LoadJpegImageDownsampled

--- gframe/image_utility.h
+++ gframe/image_utility.h
@@ -11,6 +11,14 @@ private:
 	static void imageScaleNNAA(irr::video::IImage* src, irr::video::IImage* dest, bool use_threading);
 public:
 	static void Resize(irr::video::IImage* src, irr::video::IImage* dest, bool use_threading);
+	/**
+	 * Decode a JPEG file using libjpeg with optional DCT-domain downscaling (1/2, 1/4, 1/8).
+	 * When targetWidth / targetHeight are provided the largest scale denominator that keeps
+	 * the decoded dimensions >= target is chosen, saving memory and CPU time.
+	 * The reader is not dropped by this function.
+	 * @return Image pointer. Must be dropped after use.
+	 */
+	static irr::video::IImage* LoadJpegImageDownsampled(irr::video::IVideoDriver* driver, irr::io::IReadFile* reader, irr::s32 targetWidth, irr::s32 targetHeight);
 };
 
 } // namespace ygo
