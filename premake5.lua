9026d4c5114f5969d775fe08f3e6256ccc68863f
mercury233 <me@mercury233.me>
2026-07-11 11:14:16 +0800

    add _WIN32_WINNT

--- premake5.lua
+++ premake5.lua
@@ -342,7 +342,8 @@ workspace "YGOPro"
     filter "system:windows"
         systemversion "latest"
         startproject "YGOPro"
-        defines { "WINVER=0x0601" } -- WIN7
+        -- Target Windows 7. Require Windows 10 SDK 1803+ to build, but the generated binary will run on Windows 7+.
+        defines { "WINVER=0x0601", "_WIN32_WINNT=0x0601" }
 
     if WIN7_SUPPORT then
         filter { "system:windows", "action:vs2026" }
