04797d86bc7dfb1fc06c885e9bb728dd975b6c4a
mercury233 <me@mercury233.me>
2026-08-05 16:35:11 +0800

    test implicit static-libgcc

--- gframe/premake5.lua
+++ gframe/premake5.lua
@@ -67,7 +67,7 @@ project "YGOPro"
 
     filter { "system:windows", "action:gmake" }
         links { "opengl32", "imm32" }
-        linkoptions { "-static", "-static-libgcc", "-static-libstdc++" }
+        linkoptions { "-static" }
 
     filter "system:macosx"
         links { "OpenGL.framework", "Cocoa.framework", "IOKit.framework", "Carbon.framework" }
